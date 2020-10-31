734f9d35b2e5fb282685035f4bbcb4250321deba
brennandmcdonald <71531277+brennandmcdonald@users.noreply.github.com>
2020-10-31 16:58:09 -0400

    detect.py

--- test.py
+++ test.py
@@ -7,10 +7,11 @@ if __name__ == '__main__':
     directory = "data/Track 10-22/"
     file = directory+"30_3.png"
     img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-    size = (int(img.shape[0]*0.3), int(img.shape[1]*0.3))
+    size = (int(img.shape[1]*0.5), int(img.shape[0]*0.5))
     output = cv2.resize(img, size)
-    test, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    thresh = cv2.adaptiveThreshold(output, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    #test, thresh = cv2.threshold(output, 75, 255, cv2.ADAPTIVE_THRESH_MEAN_C)
+    cv2.imwrite("output_1.png", thresh)
     detector.detect(thresh, return_image=True)
-    cv2.imwrite("output_1.jpg", thresh)
     cap.release()
     cv2.destroyAllWindows()
